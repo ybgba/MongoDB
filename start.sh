5d802af24b2e20b5e6db6e0d72c087f78035dfe3
OldYang <ybgba@163.com>
2018-06-21 18:22:53 +0800

    startup script

---
+++ start.sh
@@ -0,0 +1,5 @@
+/mnt/mongo364/bin/mongod -f /mnt/mongo364/etc/mongod_27059.cnf
+/mnt/mongo364/bin/mongod -f /mnt/mongo364/etc/mongod_27058.cnf
+/mnt/mongo364/bin/mongod -f /mnt/mongo364/etc/mongod_27057.cnf
+/mnt/mongo364/bin/mongod -f /mnt/mongo364/etc/mongod_27056.cnf
+/mnt/mongo364/bin/mongos -f /mnt/mongo364/etc/mongos.cnf
